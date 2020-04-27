f619b4af71e9fecb5d825623edbb02881be59f20
Borja Rodrigo <zordycorak@gmail.com>
2020-04-27 14:12:37 +0200

    Initial setup

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <div class="container"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App"
+};
+</script>
+
+<style lang="scss">
+@import "@/assets/sass/variables/_colors.scss";
+
+#app {
+  font-family: "Barlow", sans-serif;
+  background-color: $dark-bg;
+}
+</style>
