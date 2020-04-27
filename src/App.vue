a251955ada589fd0a59b6ef7c71bb1eedaed1c82
Borja Rodrigo <zordycorak@gmail.com>
2020-04-28 00:00:51 +0200

    Added Nav component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <template>
   <div id="app">
-    <div class="container"></div>
+    <div class="container">
+      <vm-nav />
+    </div>
   </div>
 </template>
 
 <script>
+import VmNav from "@/components/VmNav.vue";
+
 export default {
-  name: "App"
+  name: "App",
+  components: {
+    VmNav
+  }
 };
 </script>
 
@@ -16,5 +23,11 @@ export default {
 #app {
   font-family: "Barlow", sans-serif;
   background-color: $dark-bg;
+  font-size: 15px;
+
+  .container {
+    padding: 13px 25px;
+    max-width: 1100px;
+  }
 }
 </style>
---
+++ src/components/VmNav.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="menu-component">
+    <vm-input icon="search" />
+    <div class="filters">
+      <vm-select icon="filter" />
+      <vm-select icon="order" />
+      <icon-random />
+    </div>
+  </div>
+</template>
+
+<script>
+import IconRandom from "@/components/Icon/IconRandom.vue";
+import VmInput from "@/components/VmInput.vue";
+import VmSelect from "@/components/VmSelect.vue";
+
+export default {
+  name: "VmNav",
+
+  components: {
+    IconRandom,
+    VmInput,
+    VmSelect
+  }
+};
+</script>
+
+<style lang="scss">
+@import "@/assets/sass/variables/_colors.scss";
+
+.menu-component {
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 35px;
+
+  .filters {
+    display: flex;
+  }
+
+  .form-item {
+    height: 20px;
+    background-color: $black;
+    border: none;
+
+    &:focus {
+      outline: none;
+    }
+  }
+}
+</style>
